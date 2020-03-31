14a730964d5f329dacb73dbb7b43455e1d22d885
Annemijn Dijkhuis <annemijndijkhuis@mbpvanannemijn.home>
2020-03-31 12:24:27 +0200

    last change example.py

--- example.py
+++ example.py
@@ -1,10 +1,4 @@
 # This is an example
 
-<<<<<<< HEAD
-print("Hello world")
-print("Lianne was here")
-=======
-
 print("blalbalbal")
 print("testtestest")
->>>>>>> 4b5a0481bb5e99560b294e797c477707a84e7ec6
