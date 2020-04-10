67437d0209e78787e8a729dcf07af9bfe4aa41ee
Carlos Hernandez <60603769+cho250@users.noreply.github.com>
2020-04-10 16:57:36 +0200

    Create Initial_Pipeline.py
    First draft of a pipeline that pretreats the data, applies a classifier and a CV method and plots the results using a confusion table. It runs but there may be some (conceptual) errors. It does NOT include any feature selection method

---
+++ Initial_Pipeline.py
@@ -0,0 +1,89 @@
+# Initial pipeline for Breast Cancer type prediction.
+# This is just a preliminary example of how we could process the data and apply a CV method + a classifying method on our data
+# and plot the results using a confusion table. I tried a randomized CV method with 25% and SVM as classifier,
+# but we can change the methods easily. Note that there IS NO feature selection method at all, this needs to be implemented.
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#This section can be pasted under any of the other subsections to generate a file containing the desired dataframe.
+# It is just a way to visualize what each step does in a nice txt format instead of the console.
+# Just paste it under the desired section and change "dftovisualize" to the df name you want to see
+"""
+open('checkpoint.txt', 'w').close()     # Deletes everything that was written in the file before
+dftovisualize.to_csv(r'checkpoint.txt', header=None, index=None, sep=' ', mode='a')
+"""
+
+
+#Storing the data as two Dataframes
+callfile = "data/Train_call.txt"
+clinicalfile = "data/Train_clinical.txt"
+dfcall = pd.read_csv(callfile, delimiter="\t")
+dfclin = pd.read_csv(clinicalfile, delimiter="\t")
+
+#Tell number of samples and variables of the dataset
+print("Dataset dimensions : {}".format(dfcall.shape))
+
+#Check whether there is any "null" or "na" in the table (there is not) so no need to print it
+dfcall.isnull().sum()
+dfcall.isna().sum()
+
+#Rotate dfcall 90 degrees. This is needed because we want to visualize information per patient.
+#To do so, data must be rotated so each row is now a patient with a specific combination of 0,1,2 values per column
+# (chromosome) and a diagnosis as last column
+
+temp_df = dfcall.T                      #Transposes the dataframe
+rotated_df=temp_df[4::]                 #Removes the first 4 lines corresponding to the chromosomal locations and clone number (we do not need them for the moment)
+rotated_df = rotated_df.reset_index()   #Sets the new index based on the new number of rows (needed for adding the Diagnosis column afterwards)
+
+
+#Add the column of the diagnosis from dfclin at the end of the rotated dfcall. Now each patient (row) has a combination
+# of 0,1,2 values, that we have to link to a diagnosis. It can be found in dfclin dataframe.
+
+final_df=rotated_df.assign(Diagnosis=dfclin.Subgroup)   #Adds a column Diagnosis with the information of dfclin "Subgroup" column
+
+
+#Store separately the values and the diagnosis. This step is needed for the classifier,
+# we need to give separately the values from the diagnosis
+
+X = final_df.iloc[:,1:2835].values      #Store in X all the row data (without sample name or diagnosis). NOTICE that this takes ALL the features, usually we would apply a feature selection method
+Y = final_df.iloc[:, -1].values         #Store in Y all the diagnosis ("Tripneg","HR+",...)
+
+
+#Divide the data into Training and Test set (25% of the data), selected randomly (for the moment).
+# This step needs to be adapted to the CV method we choose.
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+#Apply Support Vector Machine Algorithm for classification using the train set. I chose this method as an example,
+# we need to apply the ones we chose in here.
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)   #This builds the classifier using the SVC method
+classifier.fit(X_train, Y_train)                        #Using the training data (X for the 0,1,2 and Y for the diagnosis associated)
+
+#Predict the test set results using SVM model
+Y_pred = classifier.predict(X_test)                     #This predicts the diagnosis (Y_pred) of the test set data (X_test)
+
+
+#Create confusion table based on the prediction values
+from sklearn.metrics import confusion_matrix
+labels=["HER2+","HR+","Triple Neg"]
+cm = confusion_matrix(Y_test, Y_pred,labels)            #This builds a confusion matrix by comparing diagnosis from the Test set (True diagnosis) against the predicted diagnosis (Y_pred)
+print(cm)
+
+#Visualize the confusion table
+import seaborn as sns
+sns.set(font_scale=1.4) # for label size
+sns.heatmap(cm, annot=True, annot_kws={"size": 16},xticklabels=labels,yticklabels=labels,cmap="Blues")
+plt.ylabel('True')
+plt.xlabel('Predicted')
+plt.show()
+
+
+"""Sources:
+https://towardsdatascience.com/building-a-simple-machine-learning-model-on-breast-cancer-data-eca4b3b99fa3
+https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea
+https://github.com/DTrimarchi10/confusion_matrix/blob/master/cf_matrix.py"""
